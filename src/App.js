63123c30b9449cdc25cf0fbafabdbd35db934fc1
Extraterrestial-Annie <j.j.karkkainen@gmail.com>
2023-05-15 22:23:50 +0300

    final

--- src/App.js
+++ src/App.js
@@ -7,51 +7,47 @@ let parsed = '';
 class App extends React.Component {
   constructor(props) {
     super(props);
-  
+
     // Initializing the state 
-    this.state = { songs: [], checked: {}  };
+    this.state = { songs: [], checked: {} };
   }
 
-  parsedict(data){
-    const dict = { ...this.state.checked};
-    console.log(data)
-    data.map( (d) => dict[d._id] = false )
-    console.log(dict);
+  parsedict(data) {
+    const dict = { ...this.state.checked };
+    //console.log(data)
+    data.map((d) => dict[d._id] = false)
+    //console.log(dict);
     return dict;
   }
 
   componentDidMount() {
-  
+
     fetch('https://annie-apiproject-1.azurewebsites.net/api/getall')
-        .then(response => response.json())
-        .then(data => 
-          this.setState({ songs: data, checked : this.parsedict(data) }));
-          //console.log(typeof(this.state.songs))
+      .then(response => response.json())
+      .then(data =>
+        this.setState({ songs: data, checked: this.parsedict(data) }));
+    //console.log(typeof(this.state.songs))
 
   }
 
-
-  handleClick = () => {
-    console.log(' clicked!!');
-  }
 
   handleCheck = (id) => {
-    console.log(id)
-    const dict = { ...this.state.checked};
+    //console.log(id)
+    const dict = { ...this.state.checked };
     dict[id] = !this.state.checked[id];
-    if(!this.state.checked[id]){
-      console.log(dict)
-      console.log("joooo")
+    if (!this.state.checked[id]) {
+      //console.log(dict)
+      //console.log("joooo")
       fetch('https://annie-apiproject-1.azurewebsites.net/api/' + id)
         .then(response => response.json())
-        .then(data => 
-          this.setState({ songs: [data], checked : dict }));
+        .then(data =>
+          this.setState({ songs: [data], checked: dict }));
     }
-    else{
+    else {
       fetch('https://annie-apiproject-1.azurewebsites.net/api/getall')
-      .then(response => response.json())
-      .then(data => 
-        this.setState({ songs: data, checked : dict }));
+        .then(response => response.json())
+        .then(data =>
+          this.setState({ songs: data, checked: dict }));
 
     }
   };
@@ -59,34 +55,47 @@ class App extends React.Component {
   render() {
 
     const songs = this.state.songs;
-    console.log(songs)
+    //console.log(songs)
     const listItems = songs.map((data) =>
-    <tr>
-    <td key={data.id} > {data.name} </td>
-    <td> {data.artist} </td>
-    <td> {data.year} </td>
-    <td> {data.album} </td>
-    <td> <input type="checkbox" onChange={() => this.handleCheck(data._id)} id={data._id} /> </td>
-    <td><span class="pure-form-message">Click to filter/unfilter</span></td>
-    </tr>
+      <tr key={data._id}>
+        <td> {data.name} </td>
+        <td> {data.artist} </td>
+        <td> {data.year} </td>
+        <td> {data.album} </td>
+      </tr>
     );
+
+    const listItems2 = songs.map((data) =>
+    <li class="pure-menu-item" key={data._id}>
+      <a href="#" onClick={() => this.handleCheck(data._id)} class="pure-menu-link">{songs.length > 1 ? data._id : "show all"}</a>
+    </li>
+  );
+
     return (
       <div>
-      <table class='pure-table pure-table-bordered'>
-      <thead>
-      <tr>
-        <th>Song name</th>
-        <th>Artist</th>
-        <th>Year</th>
-        <th>Album</th>
-        <th>Single Out</th>
-        <th>Help text</th>
-      </tr>
-    </thead>
-    <tbody>
-    {listItems}
-    </tbody>
-      </table>
+        <div class="pure-menu pure-menu-horizontal">
+          <ul class="pure-menu-list">
+            <li class="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
+              <a href="#" id="menuLink1" class="pure-menu-link">Select ID</a>
+              <ul class="pure-menu-children">
+              {listItems2}
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <table class='pure-table pure-table-bordered'>
+          <thead>
+            <tr>
+              <th>Song name</th>
+              <th>Artist</th>
+              <th>Year</th>
+              <th>Album</th>
+            </tr>
+          </thead>
+          <tbody>
+            {listItems}
+          </tbody>
+        </table>
       </div>
     );
   }
